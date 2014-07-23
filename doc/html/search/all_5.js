4dbd1625ed959ae445a7e7242bf40089af62b4f6
Benjamin <blasterbug@users.noreply.github.com>
2014-07-23 12:06:25 +0200

    adding doc

---
+++ doc/html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['object_5fdetected',['object_detected',['../da/d03/camduino_8c.html#a655dc011feef1f3d771a0b24a1f69e17',1,'object_detected():&#160;camduino.c'],['../d2/d36/camduino_8h.html#a655dc011feef1f3d771a0b24a1f69e17',1,'object_detected():&#160;camduino.c']]]
+];
---
+++ doc/html/search/variables_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['y',['y',['../d3/d90/structposition.html#ab7163210f8aa5e8dc68ef434a315792c',1,'position']]]
+];
